44de3acc56f24bc4f6a548a85b052283ed0d0299
j <j.stud.res@gmail.com>
2018-03-14 02:06:27 +0200

    update project config;

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'inline-source-map'
+  entry: './src/index.ts',
+  devtool : 'inline-souce-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins : [
+    new HtmlWebpackPlugin({
+      title: "Template",
+      filename: "index.html"
+    })
+  ]
 };
